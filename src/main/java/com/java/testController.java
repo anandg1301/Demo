21277364b735de9084fca63cb9b88c7f29efa299
Srivatsan1988 <58552415+Srivatsan1988@users.noreply.github.com>
2020-09-17 22:53:30 +0530

    Create testController.java

---
+++ src/main/java/com/java/testController.java
@@ -0,0 +1,36 @@
+package com.java;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class testController {
+	String message = "Welcome to Spring MVC!";
+
+	@RequestMapping(value="/hello",method = RequestMethod.GET)
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+	
+	@RequestMapping(value="/index")
+	public ModelAndView index() {
+		 ModelAndView mav = new ModelAndView("Welcome");  
+		    return mav; 
+	
+	}
+	
+	@RequestMapping("/")
+	public String getFirstPage()
+	{
+		return "hello";
+	}
+}
